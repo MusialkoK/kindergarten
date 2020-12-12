63ac6f1932c169833bf009ad9c7b4423ad11acc9
Kamil <k.musialkowski@gmail.com>
2020-12-12 10:50:17 +0100

    Initial commit

---
+++ src/main/java/pl/com/happyhouse/krzeptow/KrzeptowApplication.java
@@ -0,0 +1,13 @@
+package pl.com.happyhouse.krzeptow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrzeptowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrzeptowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/com/happyhouse/krzeptow/KrzeptowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.happyhouse.krzeptow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrzeptowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

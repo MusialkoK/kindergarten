8c44868cf332940e4948b8a4b1a5e29724c40feb
Kamil <k.musialkowski@gmail.com>
2020-12-14 23:11:08 +0100

    Security re-configure

--- src/main/java/pl/com/happyhouse/krzeptow/security/AppUserDetails.java
+++ src/main/java/pl/com/happyhouse/krzeptow/security/AppUserDetails.java
@@ -2,31 +2,31 @@ package pl.com.happyhouse.krzeptow.security;
 
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import pl.com.happyhouse.krzeptow.model.User;
 
 import java.util.Collection;
+import java.util.List;
 
 @AllArgsConstructor
 public class AppUserDetails implements UserDetails {
 
-//    private final User user;
+    private final User user;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return List.of(new SimpleGrantedAuthority(user.getRole().getRoleName()));
-        return null;
+        return List.of(new SimpleGrantedAuthority(user.getRole().getRoleName()));
     }
 
     @Override
     public String getPassword() {
-//        return user.getPassword();
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-//        return user.getEmail();
-        return null;
+        return user.getEmail();
     }
 
     @Override
--- src/main/java/pl/com/happyhouse/krzeptow/security/AppUserDetailsService.java
+++ src/main/java/pl/com/happyhouse/krzeptow/security/AppUserDetailsService.java
@@ -5,19 +5,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import pl.com.happyhouse.krzeptow.model.User;
+import pl.com.happyhouse.krzeptow.repository.UserRepository;
 
 @AllArgsConstructor
 @Service
 public class AppUserDetailsService implements UserDetailsService {
 
-    //private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-//        User user = userRepository.findByEmail(s).get();
-//        return new AppUserDetails(user);
-
-        return null;
+        User user = userRepository.findByEmail(s).get();
+        return new AppUserDetails(user);
     }
 
 

e03e714ece3e63be3f42a094551785503be4c35c
Kamil <k.musialkowski@gmail.com>
2021-01-09 12:22:47 +0100

    creating calendars dynamically

--- src/main/resources/static/js/dates.js
+++ src/main/resources/static/js/dates.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
-    $('#mdp-demo').multiDatesPicker({
-        beforeShowDay: $.datepicker.noWeekends,     // Disable selection of weekends
-        firstDay: 1,                                // Start with Monday
-        minDate: new Date(),                       // prevent selection of date older than today
-        onSelect: function () {
-            $("#DatesLabel").val(JSON.stringify($(this).multiDatesPicker("getDates")))
-        }
-    });
+    const childrenCount = document.getElementById('childrenCount').value;
+    let i;
+    for (i = 0; i < childrenCount; i++) {
+        $('#mdp-demo' + i).multiDatesPicker({
+            beforeShowDay: $.datepicker.noWeekends,     // Disable selection of weekends
+            firstDay: 1,                                // Start with Monday
+            minDate: new Date(),                       // prevent selection of date older than today
+            onSelect: function () {
+                $("#DatesLabel+i").val(JSON.stringify($(this).multiDatesPicker("getDates")))
+            }
+        });
+    }
 });
-
-
